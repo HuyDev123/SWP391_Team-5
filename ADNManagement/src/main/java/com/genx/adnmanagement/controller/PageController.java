40a5df74a2b0531b9e170ea3b2f5233319298334
Kuro-496 <135246love@gmail.com>
2025-06-21 22:07:02 +0700

    complete login

--- ADNManagement/src/main/java/com/genx/adnmanagement/controller/PageController.java
+++ ADNManagement/src/main/java/com/genx/adnmanagement/controller/PageController.java
@@ -3,47 +3,39 @@ package com.genx.adnmanagement.controller;
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
     @GetMapping({"/", "/home"})
-    public String index(HttpSession session, Model model) {
-        Object user = session.getAttribute("user");
-        model.addAttribute("user", user);
+    public String index() {
         return "index"; // trả về templates/index.html
     }
 
     @GetMapping("/booking")
-    public String booking(HttpSession session, Model model) {
-        Object user = session.getAttribute("user");
-        model.addAttribute("user", user);
+    public String booking() {
         return "booking"; // trả về templates/booking.html
     }
 
     @GetMapping("/services")
-    public String services(HttpSession session, Model model) {
-        Object user = session.getAttribute("user");
-        model.addAttribute("user", user);
+    public String services() {
         return "services"; // trả về templates/services.html
     }
 
     @GetMapping("/knowledge")
-    public String knowledge(HttpSession session, Model model) {
-        Object user = session.getAttribute("user");
-        model.addAttribute("user", user);
+    public String knowledge() {
         return "knowledge"; // trả về templates/knowledge.html
     }
 
     @GetMapping("/sampling-guide")
-    public String samplingGuide(HttpSession session, Model model) {
-        Object user = session.getAttribute("user");
-        model.addAttribute("user", user);
+    public String samplingGuide() {
         return "sample-collection"; // trả về templates/sample-collection.html
     }
     @GetMapping("/login")
-    public String logIn() {
+    public String logIn(HttpSession session) {
+        if (session.getAttribute("user") != null) {
+            return "redirect:/home";
+        }
         return "login"; // trả về templates/login.html
     }
 }
